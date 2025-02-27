2c7af40f030cfde887117949ac1a291234ad6a27
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-27 16:39:27 +0100

    add function generate email list

--- main.js
+++ main.js
@@ -8,19 +8,32 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 const listEl = document.querySelector('.list-group')
 const inputEl = document.getElementById('GENERA')
 
-for (let i = 0; i < 10; i++){
+// generazione e-mail list 
 
-    fetch ('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(response => response.json())
-    .then (data => {
-        console.log(data, data.response);
+function generateEmailList (n){
+
+    const arrayEmailList = []
+
+    for (let i = 0; i < n; i++){
+
+        fetch ('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => response.json())
+        .then (data => {
+            console.log(data, data.response);
+        
+            const randomEmail = data.response
+            console.log(randomEmail);
     
-        const randomEmail = data.response
-        console.log(randomEmail);
-
-        listEl.innerHTML += `<li class="list-group-item">${randomEmail}</li>`
-    })
+            arrayEmailList.push(randomEmail) 
+        })
+    }
+    return arrayEmailList
 }
+const emailList = generateEmailList(10)
+console.log(emailList);
+
+listEl.innerHTML += `<li class="list-group-item">${emailList}</li>`
+
 
 inputEl.addEventListener('click', function(){
 
