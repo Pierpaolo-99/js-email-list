538e13a4fd350d1ab16a0ecc107e780969c5c639
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-27 15:50:48 +0100

    add style with bootstrap and add input event listener

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)*/
 
 const listEl = document.querySelector('.list-group')
+const inputEl = document.getElementById('GENERA')
 
 for (let i = 0; i < 10; i++){
 
@@ -20,4 +21,22 @@ for (let i = 0; i < 10; i++){
         listEl.innerHTML += `<li class="list-group-item">${randomEmail}</li>`
     })
 }
+
+inputEl.addEventListener('click', function(){
+
+
+    for (let i = 0; i < 10; i++){
+
+        fetch ('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => response.json())
+        .then (data => {
+            console.log(data, data.response);
+        
+            const randomEmail = data.response
+            console.log(randomEmail);
+    
+            listEl.innerHTML += `<li class="list-group-item">${randomEmail}</li>`
+        })
+    }
+})
 
